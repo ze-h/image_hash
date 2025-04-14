e3050852fba86ea69bea924226c21f48aa658b48
ze-h <ziadhefnawy@gmail.com>
2025-04-14 18:38:43 -0400

    app first iteration

---
+++ app.py
@@ -0,0 +1,36 @@
+import cv2
+import sys
+import rembg
+import hashlib
+
+if len(sys.argv) < 2:
+    print("{} needs at least one argument".format(sys.argv[0]))
+    exit(1)
+
+# import image and remove bg
+imagebg = cv2.imread(sys.argv[1])
+cv2.imwrite("nobg.jpg", rembg.remove(imagebg))
+image = cv2.imread("nobg.jpg")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+_, thresholded = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# draw contours and save
+cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+cv2.imwrite("contours.jpg", image)
+
+# truncate contours into single string
+contourtxt = ""
+for contour in contours:
+    for line in contour:
+        for point in line:
+            contourtxt += str(point[0] * point[1])
+
+# hash to 256, save
+h = hashlib.sha256()
+h.update(bytes(contourtxt.encode()))
+with open("contours.txt", "wb") as file:
+    file.write(h.digest())
