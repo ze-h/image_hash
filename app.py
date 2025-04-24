0c55e8eda05d1ac4101d440d480ecf15d1ffdcc1
ze-h <ziadhefnawy@gmail.com>
2025-04-23 23:36:06 -0400

    changes from vis to app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import cv2
 import sys
-import rembg
 import hashlib
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
 
 if len(sys.argv) < 2:
     print("{} needs at least one argument".format(sys.argv[0]))
@@ -13,19 +13,34 @@ if len(sys.argv) < 2:
     )
     exit(1)
 
+# initialize the face recognizer (default face haar cascade)
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier(
+    os.path.abspath("cascades/haarcascade_frontalface_default.xml")
+)
+
+# eye recognizer
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier(os.path.abspath("cascades/haarcascade_eye.xml"))
+
 # import image and remove bg
-imagebg = cv2.imread(sys.argv[1])
-cv2.imwrite("nobg.jpg", rembg.remove(imagebg))
-image = cv2.imread("nobg.jpg")
+image = cv2.imread(sys.argv[1])
 
+# Detects faces of different sizes in the input image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+(x, y, w, h) = faces[0]
 
+# To draw a rectangle in a face
+cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+roi_gray = gray[y:y+h, x:x+w]
+roi_color = image[y:y+h, x:x+w]
+blur = cv2.blur(roi_gray, (10, 10))
 _, thresholded = cv2.threshold(blur, 85, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # draw contours and save
-cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+cv2.drawContours(roi_color, contours, -1, (0, 255, 0), 2)
 cv2.imwrite("contours.jpg", image)
 
 # take sum of point products
@@ -82,7 +97,7 @@ if len(sys.argv) > 3:
             out.write(plaintext)
         f.close()
         exit(0)
-        
+
     # neither, panic
     f.close()
     print("encryption/decryption needs to be specified")
